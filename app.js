0f5be10961b0c274c0c2486bb0a1ba7549dc9e12
guneysrhat <guneysrhat@gmail.com>
2022-10-01 16:56:59 +0300

    clock done

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const clock =()=> {
-    const data = new Date()
-}
+let pm = true;
+document.querySelector("button").onclick = () => {
+  pm = !pm;
+  const clock = () => {
+    const date = new Date();
+    let hours = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    hours = hours < 10 ? "0" + hours : hours;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    if (pm) {
+      hours = hours - 12;
+      document.querySelector("button").innerText = "BACK";
+      document.getElementById(
+        "MyClockDisplay"
+      ).innerText = `${hours}:${minute}:${second} PM`;
+    } else {
+      document.querySelector("button").innerText = "PM";
+      document.getElementById(
+        "MyClockDisplay"
+      ).innerText = `${hours}:${minute}:${second}`;
+    }
+  };
+  setInterval(() => {
+    clock();
+  }, 1000);
+};
